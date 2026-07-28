ae427a6a6bdcf954005a8e69c6f4ee685048ed1f
Konstantin Kapitonov <k.kapitonov@2gis.ru>
2026-07-28 18:00:12 +0300

    Initial Commit

---
+++ PhotoScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoScroll
+//
+//  Created by Капитонов Константин Евгеньевич on 28.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
