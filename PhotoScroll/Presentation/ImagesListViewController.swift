d8cc07caf474b54e7138a38bed3ecd5dc6eaac07
Konstantin Kapitonov <117959636+konstantin-kapitonov@users.noreply.github.com>
2026-08-18 09:56:05 +0300

    Решение задачи из спринта 9 (#2)
    Решение задачи из спринта 9

--- PhotoScroll/Presentation/ImagesListViewController.swift
+++ PhotoScroll/Presentation/ImagesListViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
+    //MARK: - Constants
+    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
+    
     // MARK: - IBOutlets
+    
     @IBOutlet private var tableView: UITableView!
 
     // MARK: - Properties
+    
     private let photosName = (0..<20).map { "\($0)" }
     
     private lazy var dateFormatter: DateFormatter = {
@@ -22,6 +28,7 @@ final class ImagesListViewController: UIViewController {
     }()
 
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,18 +36,41 @@ final class ImagesListViewController: UIViewController {
     }
     
     // MARK: - Private Methods
+    
     private func configCell(for cell: ImagesListCell, at index: Int) {
         guard let image = UIImage(named: photosName[index]) else {
             return
         }
-
+        
+        cell.selectionStyle = .none
         cell.photoView.image = image
         cell.imageDateLabel.text = dateFormatter.string(from: Date())
-        cell.likeButton.setImage(index % 2 == 0 ? UIImage.active : UIImage.noActive, for: .normal)
+        cell.likeButton.setImage(index % 2 == 0 ? UIImage.likeActive : UIImage.likeNoActive, for: .normal)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+
+            guard let image = UIImage(named: photosName[indexPath.row]) else {
+                assertionFailure("Image not found")
+                return
+            }
+            viewController.configure(with: image)
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
 // MARK: - UITableViewDataSource
+
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         photosName.count
@@ -59,6 +89,7 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 // MARK: - UITableViewDelegate
+
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -69,5 +100,11 @@ extension ImagesListViewController: UITableViewDelegate {
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let scale = imageViewWidth / image.size.width
         return image.size.height * scale + imageInsets.top + imageInsets.bottom
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.setSelected(false, animated: false)
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
 }
---
+++ PhotoScroll/Presentation/ProfileViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ProfileViewController.swift
+//  PhotoScroll
+//
+//  Created by Капитонов Константин Евгеньевич on 11.08.2026.
+//
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    // MARK: - Properties
+    private lazy var imageView = UIImageView()
+    private lazy var exitButton = UIButton()
+    private lazy var nameLabel = UILabel()
+    private lazy var userNameLabel = UILabel()
+    private lazy var descriptionLabel = UILabel()
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypBlack
+        
+        configureProfileImageView()
+        configureExitButton()
+        configureNameLabel()
+        configureUserNameLabel()
+        configureDescriptionLabel()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureProfileImageView() {
+        imageView.image = .profile
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        
+        [
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        ].forEach { $0.isActive = true }
+        
+        imageView.layer.cornerRadius = 35
+        imageView.layer.masksToBounds = true
+    }
+    
+    private func configureExitButton() {
+        exitButton.setImage(.exit, for: .normal)
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+
+        [
+            exitButton.widthAnchor.constraint(equalToConstant: 44),
+            exitButton.heightAnchor.constraint(equalToConstant: 44),
+            exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ].forEach { $0.isActive = true }
+        
+        exitButton.addTarget(
+            self,
+            action: #selector(onExitButtonTap),
+            for: .touchUpInside
+        )
+    }
+    
+    private func configureNameLabel() {
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        nameLabel.textColor = .ypWhite
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        [
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+        ].forEach { $0.isActive = true }
+    }
+    
+    private func configureUserNameLabel() {
+        userNameLabel.text = "@ekaterina_nov"
+        userNameLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        userNameLabel.textColor = .ypGray
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userNameLabel)
+        
+        [
+            userNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            userNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+        ].forEach { $0.isActive = true }
+    }
+    
+    private func configureDescriptionLabel() {
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        descriptionLabel.textColor = .ypWhite
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        [
+            descriptionLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+        ].forEach { $0.isActive = true }
+    }
+    
+    @objc private func onExitButtonTap(_ sender: UIButton) {
+        
+    }
+}
